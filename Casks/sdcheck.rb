7c9ef8f2a912060d0c5999e963f7bb794bd0597c
repomonkey <repomonkey@gmail.com>
2026-04-18 17:42:32 +1000

    Fix sha256 to match uploaded DMG

--- Casks/sdcheck.rb
+++ Casks/sdcheck.rb
@@ -1,6 +1,6 @@
 cask "sdcheck" do
   version "1.0.0"
-  sha256 "ee610e0be54fb4974a29cb6489cb0cfba2fac3332952aa963e13817137d3b11e"
+  sha256 "aaabbbf8b128dfe4ea94a8cc62e80f78e8195bba2d7bd6f893bbb0e94333087b"
 
   url "https://andyhutchinson.com.au/SDCheck-#{version}.dmg"
   name "SD-Check"
