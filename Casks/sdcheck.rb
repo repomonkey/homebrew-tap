63d91ce4328d11fadfaa4ee7ab52d078fd4a83c8
repomonkey <repomonkey@gmail.com>
2026-04-18 17:41:29 +1000

    Add sdcheck cask

---
+++ Casks/sdcheck.rb
@@ -0,0 +1,11 @@
+cask "sdcheck" do
+  version "1.0.0"
+  sha256 "ee610e0be54fb4974a29cb6489cb0cfba2fac3332952aa963e13817137d3b11e"
+
+  url "https://andyhutchinson.com.au/SDCheck-#{version}.dmg"
+  name "SD-Check"
+  desc "Menubar SD card health checker for photographers"
+  homepage "https://andyhutchinson.com.au"
+
+  app "SDCheck.app"
+end
